3853171d0c1a12486dfb298a4f7e057531170c84
maanualonso <75090080+maanualonso@users.noreply.github.com>
2020-11-26 18:45:56 +0100

    Add files via upload

---
+++ Clases.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 18:42:26 2020
+
+@author: manus
+"""
+
+import random
+class Interfaz():
+    muertos = 0
+    salvados = 0
+    total = 
+    def vivos:
+        vivos = 
